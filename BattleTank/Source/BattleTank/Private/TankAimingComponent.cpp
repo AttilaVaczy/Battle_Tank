cba809bfbb82da1bb2ee4fe065d780a5b6c010e7
attila vaczy <attila.vaczy@yahoo.com>
2018-07-06 17:45:03 +0200

    first iteration of a moving turret implemented

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -14,11 +14,7 @@
 // Sets default values for this component's properties
 UTankAimingComponent::UTankAimingComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
 void UTankAimingComponent::SetBarrelReference(UTankBarrel* BarrelToSet)
@@ -70,4 +66,5 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection) {
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
 
 	Barrel->Elevate(DeltaRotator.Pitch);
+	Turret->Rotate(DeltaRotator.Yaw);
 }
